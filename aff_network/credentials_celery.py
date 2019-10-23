621a01cadd57812b1f2573e6867e42a9a7379b8b
green <grifon12358@gmail.com>
2019-10-23 17:34:45 +0300

    Fix

--- aff_network/credentials_celery.py
+++ aff_network/credentials_celery.py
@@ -1,5 +1,4 @@
-#CELERY_BROKER = 'amqp://guest:guest@localhost:5672//'
-# CELERY_BROKER = 'redis://localhost:6379/0'
+#CELERY_BROKER = 'redis://localhost:6379/0'
 CELERY_BROKER = 'amqp://guest:guest@localhost:5672//'
 
 BROKER_TRANSPORT_OPTIONS = {"max_retries": 3, "interval_start": 0, "interval_step": 0.2, "interval_max": 0.5}
